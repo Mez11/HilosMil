61a4e5fe3f2f11386e91a36a3e9f9698f2001fee
Joscelyn Meztli <meztli.swim@gmail.com>
2016-11-14 00:35:15 -0600

    Cambia el programa principal
    De: HiloInterrupciones a Principal.java

--- src/com/main/Principal.java
+++ src/com/main/Principal.java
@@ -5,11 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Principal {
-	
 	private static final Logger logger = LoggerFactory.getLogger( Principal.class );
 	
 	public static void main(String[] args) {
-		logger.info( "Esto es una impresion" );
+		logger.info( "Se llamara al hilo principal..." );
+		HiloInterrupciones hiloInterrupciones= new HiloInterrupciones("Hilo_Interrupciones");
+    	hiloInterrupciones.inicializarHilo( );
 		
 	}//fin main
 }//fin class
