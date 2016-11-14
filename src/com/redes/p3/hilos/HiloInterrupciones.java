bec98cac5514189c76df2870f977de973c61dd66
Joscelyn Meztli <meztli.swim@gmail.com>
2016-11-14 01:09:18 -0600

    Borra los metodos de crearLog

--- src/com/redes/p3/hilos/HiloInterrupciones.java
+++ src/com/redes/p3/hilos/HiloInterrupciones.java
@@ -15,11 +15,10 @@ public class HiloInterrupciones implements Runnable {
 	    private String nombreHilo;
 
 	    public HiloInterrupciones(String nombre){
-	    //VHiloI.setVisible(true);
-		nombreHilo=nombre;
-		estado="Nacido";
-		//VHiloI.jTextArea1.append("Creando "+nombreHilo+"\n");
-	        crearLog("");
+		    //VHiloI.setVisible(true);
+			nombreHilo=nombre;
+			estado="Nacido";
+			//VHiloI.jTextArea1.append("Creando "+nombreHilo+"\n");
 	    }
 	    
 	    public void inicializarHilo( ){
@@ -31,16 +30,6 @@ public class HiloInterrupciones implements Runnable {
 	    	hilo = new Thread(this, nombreHilo);
 	    	//y se llama a su metodo "start"
 	    	hilo.start ();
-	    }
-	    
-		//Nombres....
-	    public void crearLog(Hilo2 hilo2) {
-	    	logger.info( "Creando Log Hilo 2: {}", hilo2.returnInfo( ) );
-	    }
-	    
-	    //Nombres...
-	    public void crearLog(Hilo1 hilo1) {
-	    	logger.info( "Creando Log Hilo 1: {}", hilo1.returnInfo( ) );
 	    }
 	    
 	    public void crearLog(String aux){
@@ -55,28 +44,28 @@ public class HiloInterrupciones implements Runnable {
 	        //VHiloI.jLabel1.setText("Actividad en HiloInterrupciones");
 	        estado="Ejecutando";
 	        //Instancias @.@. de hilos
-	        Hilo1 hilo1 = new Hilo1( "Hilo_Secuencia1");
-	        Hilo2 hilo2 = new Hilo2( "Hilo_Secuencia2");
-	        crearLog(hilo1);
-	        crearLog(hilo2);
+	        Hilo1 hilo1 = new Hilo1( "Hilo_Secuencia1" );
+	        Hilo2 hilo2 = new Hilo2( "Hilo_Secuencia2" );
+	        logger.info( "Hilo 1: {}", hilo1.returnInfo( ) );
+	        logger.info( "Hilo 2: {}", hilo2.returnInfo( ) );
 	    	hilo1.start();
 	        hilo2.start();
-	        crearLog(hilo1);
-	        crearLog(hilo2);
+	        logger.info( "Hilo 1: {}", hilo1.returnInfo( ) );
+	        logger.info( "Hilo 2: {}", hilo2.returnInfo( ) );
 	        while(true){
 	            System.out.print("");
 	            if(hilo1.returnTiempo()>=2000){
 	               // VHiloI.jTextArea1.append("Interrupcion "+hilo1.returnName()+hilo1.returnEjecutando()+"\n");
 	                hilo1.pause();
 	                hilo1.setTiempo();
-	                crearLog(hilo1);
+	                logger.info( "Hilo 1: {}", hilo1.returnInfo( ) );
 	                crearLog("hilo1 "+hilo1.returnNi());
 	            }
 	            if(hilo2.returnTiempo()>=3000){
 	                //VHiloI.jTextArea1.append("Interrupcion "+hilo2.returnName()+hilo2.returnEjecutando()+"\n");
 	                hilo2.pause();
 	                hilo2.setTiempo();
-	                crearLog(hilo2);
+	                logger.info( "Log Hilo 2: {}", hilo2.returnInfo( ) );
 	                crearLog("hilo2 "+hilo2.returnNi());
 	            }
 	            //Numero de repeticiones de hilos 
@@ -87,8 +76,8 @@ public class HiloInterrupciones implements Runnable {
 	        }
 	        //VHiloI.jTextArea1.append("Termina "+nombreHilo+"\n");
 	        estado="Finalizo";
-	        crearLog(hilo1);
-	        crearLog(hilo2);
+	        logger.info( "Hilo 1: {}", hilo1.returnInfo( ) );
+	        logger.info( "Hilo 2: {}", hilo2.returnInfo( ) );
 	        crearLog("Numero maximo de interrupciones alcanzado");
 	    }
 }
